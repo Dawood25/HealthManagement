52ad0eedc2c654755265e033ebec001e482fff1b
Hatim <harajmast9008@gmail.com>
2023-04-03 13:04:32 +0000

    Login Doctor Working ,staff working,but in patient canot login becuz of id patient url

--- src/pages/DoctorPage/DoctorPage.jsx
+++ src/pages/DoctorPage/DoctorPage.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import PatientsList from "../../component/PatientList";
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
 
 const DoctorPage = (props) => {
-  return <PatientsList />;
+  const navigate = useNavigate();
+
+  const location = useLocation().state 
+ 
+
+  return (
+    <div>
+           <PatientsList />
+     </div>
+  )
+
 };
 
 export default DoctorPage;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,82 +1,92 @@
 import React from "react";
 import { LoginForm } from "../../component/LoginForm";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Category } from "../../component/Category";
 import "./Login.css";
 import firebase from "../../firebase";
 import { useNavigate } from "react-router-dom";
 
 
-const verifyCredentials = async (user,email, password) => {
-   const db = firebase.firestore();
-  const usersRef = db.collection(user);
-  const query = usersRef.where("email", "==", email).where("password", "==", password);
-  const querySnapshot = await query.get();
-
-  
-  if (!querySnapshot.exists) {
-
-    // User exists with the given email and password
-    return querySnapshot.docs[0].data();
-  } else {
-    // No user exists with the given email and password
-    console.log("Invalid email or password");
-    return false;
-  }
-};
-
 
 const Login = () => {
   const navigate = useNavigate();
   const [category, setCategory] = useState("Patient");
-  const [data,setData] = useState(null);
-  const [isLoggedIn,setIsLoggedIn] = useState(false)
+  const [loggedIn,setIsLoggedIn] = useState(false)
+  const [doctorData, setDoctorData] = useState(null);
   const [userDetails, setUserDetails] = useState({ email: "", password: "" });
+
+  useEffect(() => {
+    const fetchDoctorData = async () => {
+      const doctorRef = await firebase
+        .firestore()
+        .collection(category)
+        .where("email", "==", userDetails.email)
+        .where("password", "==", userDetails.password)
+        .get();
+
+      if (!doctorRef.empty) {
+        setDoctorData(doctorRef.docs[0]);
+      } else {
+        setDoctorData(null);
+      }
+    };
+
+    if (userDetails.email && userDetails.password) {
+      fetchDoctorData();
+    }
+  }, [userDetails]);
+
   const onChangeCategoryHandler = (e) => {
     setCategory(e.target.text);
   };
 
-    const onChangeLoginFormHandler = (event) => {
-   
-    
+  const onChangeLoginFormHandler = (event) => {
+
+
     let userDetail = {
       email: userDetails.email,
       password: userDetails.password,
     };
-  
+
     if (event.target.type == "email") {
       userDetail.email = event.target.value;
     } else {
       userDetail.password = event.target.value;
     }
-    
+
     setUserDetails(userDetail);
- 
+
   };
 
   const onSubmit = (e) => {
+    e.preventDefault();
 
-    
-   verifyCredentials(category,userDetails['email'],userDetails['password'])
-  .then((data) => {
-  console.log(data);
-  setIsLoggedIn(true)
-   setData(data);
-  })  
-  navigate("/staff",{state:{data:data,isLoggedIn:isLoggedIn}})
+    if (doctorData) {
+      console.log(doctorData)
+      navigate(`/${category}`)
+
+    } else {
+      alert("Invalid email or password for Doctor account.");
+    }
+   
+      
+
   }
   return (
-    <>
+    <> {
       <div className="container login-container">
         <div className="row justify-content-center">
           <div className="col-lg-6 col-md-8 col-sm-10">
             <Category user={category} onChangeHandler={onChangeCategoryHandler} />
             <div className="my-4">
-              <LoginForm  userDetail={userDetails} onSubmit={onSubmit} onChangeHandler={onChangeLoginFormHandler} />
+              <LoginForm userDetail={userDetails} onSubmit={onSubmit} onChangeHandler={onChangeLoginFormHandler} />
             </div>
           </div>
         </div>
       </div>
+
+    }
+      
     </>
   );
 };
